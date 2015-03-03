63833a3e5108f837368162ad534d5471bee6e902
rgarcia <rgarcia@C034-12.rouviere-gretavarmed.fr>
2015-03-03 15:44:12 +0100

    mise a jour 1

--- config/conf.php
+++ config/conf.php
@@ -1,5 +1,4 @@
 <?php
 include_once(RACINE.'/config/define.php');
-?>
 
 
--- includes/function_geocoding.php
+++ includes/function_geocoding.php
@@ -15,14 +15,14 @@ function initialize() {
   var mapOptions = {
     zoom: 9,
     center: latlng
-  }
+  };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 function codeAddress() {
   var address = document.getElementById('address').value;
   geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
+    if (status === google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
           map: map,
